d7bc7c9b7369cfc412efdbccd1ed67c6a529156a
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2022-09-10 03:15:33 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Is always 0
+ *
+ */
+int main(void)
+{
+	unsigned char a = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i);
+	a++
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+	putchar('0' + a);
+	a++
+	}
+	putchar('\n');
+	return (0);
+}
