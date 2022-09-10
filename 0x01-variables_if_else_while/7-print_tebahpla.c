410d86212c9a7bf18ea27ddcf0a1a693f4d3f24f
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2022-09-10 02:42:05 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Is always 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'z'; a--)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
