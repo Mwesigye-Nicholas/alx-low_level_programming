be185bf6cdf62edcb468c5903dbfe8d5d1edb098
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2022-09-19 12:02:40 -0700

    my commit

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,16 +1,18 @@
 #include "main.h"
-
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
+	int times_table;
+	
 	print_times_table(3);
 	_putchar('\n');
 	print_times_table(5);
-	_putchar('\n');
+	putchar('\n');
 	print_times_table(98);
 	_putchar('\n');
 	print_times_table(12);
---
+++ 0x05-pointer_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The folder contains skills for string, array nd pointer manipulation
