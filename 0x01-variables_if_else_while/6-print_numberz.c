b8d65cbdfaa7cd218a75c938e5e5ba07267b641e
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-12-16 09:54:03 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
