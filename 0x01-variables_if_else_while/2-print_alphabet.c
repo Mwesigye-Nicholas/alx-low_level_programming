62a89d6c7a64d2cf4da782bec5445b8032592b8e
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-12-16 07:54:01 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this our entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
