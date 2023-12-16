e01ad75a9f9f4e316084e85fa5ab17fcff1e76a5
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-12-16 10:10:24 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+}
