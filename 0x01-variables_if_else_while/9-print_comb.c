3eab1aa3dec5c4c10ac33abe50c2922e2ebf51e9
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-12-17 00:30:44 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - this the entry point
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
