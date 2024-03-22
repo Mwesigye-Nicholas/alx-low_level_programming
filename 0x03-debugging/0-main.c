7a1fa2b6fa04c6d7c90ddadd1da7f54e94f37a39
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2024-03-22 05:30:25 -0700

    task 0 and requirements

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests the function positive_or_negative
+ *
+ * Return: returns un integer
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
