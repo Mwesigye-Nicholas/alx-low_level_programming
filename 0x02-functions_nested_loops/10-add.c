15a05788c021cf973985562c1569b203d1702bf1
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2024-03-23 09:03:55 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @a: parameter to be added
+ *
+ * @b: paramater to be added
+ *
+ * Return: sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
