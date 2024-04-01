77c158307c0f8d2a67f5992ce75ffa52a51fc478
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2024-04-01 03:31:53 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * @void: no parameter taken
+ *
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	if (i >= 48 && i <= 57)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
